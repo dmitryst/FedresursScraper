1f59544576178b6a937040e57710c66a3c7d725c
Dmitry Stepanov <dmitry_stepanov@inbox.ru>
2025-08-03 10:38:44 +0300

    refactor: add webdriverfactory, download chromedriver to docker container

--- FedresursScraper/LotInfo.cs
+++ FedresursScraper/LotInfo.cs
@@ -1,48 +1,45 @@
-namespace FedResursScraper
+/// <summary>
+/// Лот
+/// </summary>
+public class LotInfo
 {
     /// <summary>
-    /// Лот
+    /// Вид торгов
     /// </summary>
-    public class LotInfo
-    {
-        /// <summary>
-        /// Вид торгов
-        /// </summary>
-        public string BiddingType  { get; set; }
+    public string BiddingType { get; set; }
 
-        /// <summary>
-        /// Категории
-        /// </summary>
-        public List<string> Categories { get; set; }
+    /// <summary>
+    /// Категории
+    /// </summary>
+    public List<string> Categories { get; set; }
 
-        /// <summary>
-        /// Наименование
-        /// </summary>
-        public string Description { get; set; }
+    /// <summary>
+    /// Наименование
+    /// </summary>
+    public string Description { get; set; }
 
-        /// <summary>
-        /// Начальная цена
-        /// </summary>
-        public decimal? StartPrice { get; set; }
+    /// <summary>
+    /// Начальная цена
+    /// </summary>
+    public decimal? StartPrice { get; set; }
 
-        /// <summary>
-        /// Шаг цены
-        /// </summary>
-        public decimal? Step { get; set; }
+    /// <summary>
+    /// Шаг цены
+    /// </summary>
+    public decimal? Step { get; set; }
 
-        /// <summary>
-        /// Ссылка на источник
-        /// </summary>
-        public string Url { get; set; }
+    /// <summary>
+    /// Ссылка на источник
+    /// </summary>
+    public string Url { get; set; }
 
-        /// <summary>
-        /// Задаток
-        /// </summary>
-        public decimal? Deposit { get; set; }
+    /// <summary>
+    /// Задаток
+    /// </summary>
+    public decimal? Deposit { get; set; }
 
-        /// <summary>
-        /// Порядок ознакомления с имуществом
-        /// </summary>
-        public string ViewingProcedure { get; set; }
-    }
+    /// <summary>
+    /// Порядок ознакомления с имуществом
+    /// </summary>
+    public string ViewingProcedure { get; set; }
 }
