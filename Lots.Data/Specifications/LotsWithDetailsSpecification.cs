b9a8bef73ae6bbd75c20cdaf57ff0360902fb6d5
Dmitry Stepanov <dmitry_stepanov@inbox.ru>
2025-08-20 23:01:02 +0300

    fix: order lots by createdAt desc

--- Lots.Data/Specifications/LotsWithDetailsSpecification.cs
+++ Lots.Data/Specifications/LotsWithDetailsSpecification.cs
@@ -10,7 +10,7 @@ public class LotsWithDetailsSpecification : Specification<Lot>
         Query
             .Include(l => l.Bidding)
             .Include(l => l.Categories)
-            .OrderByDescending(l => l.Bidding.AnnouncedAt)
+            .OrderByDescending(l => l.Bidding.CreatedAt)
             .Skip((pageNumber - 1) * pageSize)
             .Take(pageSize);
     }
